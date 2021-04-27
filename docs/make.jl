3b56d1a7c68607035d3319f5637dac142f170a75
Yi-Kun Yang <ykyang@gmail.com>
2021-04-27 08:30:18 -0500

    Add empty sections to index.md

---
+++ docs/make.jl
@@ -0,0 +1,5 @@
+# Run this file from docs/
+# by julia make.jl
+using Documenter, Classic
+
+makedocs(sitename="My Documentation")
---
+++ docs/src/index.md
@@ -0,0 +1,11 @@
+# Classic.jl
+
+
+
+## Node
+
+```@docs
+DijkstraNode
+```
+
+## Edge
